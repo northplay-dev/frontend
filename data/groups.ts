4999728abef3222a89e36c102cf6668d171f7075
Linux User <ryan@front.northplay.app>
2023-03-07 11:09:02 +0000

    init

---
+++ data/groups.ts
@@ -0,0 +1,16 @@
+import { Group } from "../types";
+
+/**
+ * This array simulates a database consisting of a list of groups.
+ * Assign users to groups in /data/users.ts
+ */
+export const groups: Group[] = [
+  {
+    id: "player",
+    name: "Player",
+  },
+  {
+    id: "admin",
+    name: "Admin",
+  },
+];
